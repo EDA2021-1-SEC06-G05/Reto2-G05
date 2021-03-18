7e8c5f14925b205e9131ff25ec79e468920a1e50
AndresLl1 <78116243+AndresLl1@users.noreply.github.com>
2021-03-17 23:37:06 -0500

    Update and rename App/controller.py to Aplicación/controller.py

---
+++ Aplicación/controller.py
@@ -0,0 +1,104 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación,
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
+import config as cf
+import model
+import csv
+
+
+"""
+El controlador se encarga de mediar entre la vista y el modelo.
+"""
+
+# Inicialización del Catálogo de libros
+
+def initCatalog(parametro):
+    """
+    Llama la funcion de inicializacion del catalogo del modelo.
+    """
+    catalog = model.newCatalog(parametro)
+    return catalog
+
+# Funciones para la carga de datos
+def crearSubLista(catalog, muestra):
+    nuevaLista = model.crearSubLista(catalog, muestra)
+    return nuevaLista
+
+def loadData(catalog):
+    """
+    Carga los datos de los archivos y cargar los datos en la
+    estructura de datos
+    """
+    loadVideos(catalog)
+    loadCategory(catalog)
+
+def loadVideos(catalog):
+
+    videosfile = cf.data_dir + 'videos-small.csv'
+    input_file = csv.DictReader(open(videosfile, encoding='utf-8'))
+    for video in input_file:
+        model.addVideo(catalog, video)
+
+def loadCategory(catalog):
+
+    categoryfile = cf.data_dir + 'category-id.csv'
+    input_file = csv.DictReader(open(categoryfile, encoding='utf-8'))
+    for category in input_file:
+        model.addCategory(catalog, category) 
+
+# Funciones de ordenamiento
+
+def sortVideos(catalog):
+
+    model.sortVideos(catalog)
+
+def selectionVideos(catalog):
+    
+    model.selectionVideos(catalog)
+
+def insertionVideos(catalog):
+    
+    model.insertionVideos(catalog)   
+
+def shellVideos(catalog):
+    
+    model.shellVideos(catalog)       
+    
+def quickVideos(catalog):
+
+    model.quickVideos(catalog)
+
+def mergeVideos(catalog):
+
+    model.mergeVideos(catalog)        
+# Funciones de consulta sobre el catálogo
+
+def getVideosByCategory(catalog, categoria):
+
+    category = model.getVideosByCategory(catalog, categoria)
+    return category
+
+def getBestVideos(catalog, number):
+
+    bestvideos = model.getBestVideos(catalog, number)
+    return bestvideos
+
